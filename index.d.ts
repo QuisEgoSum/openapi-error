a84c540c8a3522182fd804bb7858d6f3f51d5a5c
QuisEgoSum <subbotin.evdokim@gmail.com>
2021-12-13 19:47:52 +0300

    Added index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,94 @@
+
+export declare class BaseError extends Error {}
+
+export type CompileSchema = {
+  title?: string,
+  httpCode?: number,
+  type?: 'object',
+  properties?: {
+    message?: {
+      type?: 'string',
+      default?: string,
+      [key: string]: any
+    },
+    code?: {
+      type?: 'integer',
+      default?: number,
+      [key: string]: any
+    },
+    error?: {
+      type?: 'string',
+      default?: string,
+      [key: string]: any
+    },
+    [key: string]: any
+  },
+  additionalProperties?: boolean,
+  required?: string[],
+  [key: string]: any
+}
+
+export type DefaultValues = {
+  message?: string,
+  code?: number,
+  error?: string,
+  [key: string]: any
+}
+
+type Schema = {
+  title: string,
+  type: 'object',
+  properties: {
+    message: {
+      type: 'string',
+      default: string,
+      [key: string]: any
+    },
+    code: {
+      type: 'integer',
+      default: number,
+      [key: string]: any
+    },
+    error: {
+      type: 'string',
+      default: string,
+      [key: string]: any
+    },
+    [key: string]: any
+  },
+  additionalProperties: boolean,
+  required: string[],
+  [key: string]: any
+}
+
+type JSONError = {
+  message: string,
+  code: number,
+  error: string,
+  [key: string]: any
+}
+
+declare class CompiledError extends BaseError {
+  static schema(): Schema
+
+  static httpCode(): number
+
+  static extends(overrideSchema: CompileSchema, defaultValues: DefaultValues): typeof CompiledError
+
+  message: string
+  code: number
+  error: string
+  additional: {[key: string]: any}
+
+  constructor(error?: DefaultValues)
+
+  schema(): Schema
+
+  httpCode(): number
+
+  toJSON(): JSONError
+}
+
+
+export declare function compile(schema: CompileSchema, defaultValues: DefaultValues): typeof CompiledError
+
